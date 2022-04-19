32f798aee1405cf8c0b005a735a9b03d1465e4f2
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-04-19 12:57:20 -0300

    :page_with_curl: Exercício 25

---
+++ exercios/ex25.js
@@ -0,0 +1,24 @@
+
+// resolução com while
+let numeros =0;
+let lista =[]
+
+function todosPares() {
+
+    while(numeros <=50)
+    numeros=+
+    lista.push(numeros)
+    console.log(lista)
+}
+
+todosPares()
+
+// resolução com o for
+
+function contador() {
+    for (let number = 1; number <=50; number++){
+        console.log(number)
+    }
+}
+ contador()
+
